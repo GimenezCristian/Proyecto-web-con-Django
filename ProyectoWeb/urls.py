175f6df828b143dcdcc55ebe0761ebb746e39053
GimenezCristian <gimenezc779@gmail.com>
2023-04-21 00:49:57 -0300

    Agrego el autor de los post y tambien las categorias

--- ProyectoWeb/urls.py
+++ ProyectoWeb/urls.py
@@ -24,5 +24,7 @@ urlpatterns = [
 
     path('servicios/', include ('servicios.urls')),
 
+    path('blog/', include ('blog.urls')),
+
     path("", include ("ProyectoWebApp.urls")),    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
+from blog.models import Categoria
 
 # Create your views here.
+
+def blog (request):
+    
+    posts=Post.objects.all()
+    categoria=Categoria.objects.all()
+
+    return render (request, "blog/blog.html", {"posts":posts,"categorias":categoria})
