93f08ef5861ee71b66817051e5bc06a4396932b4
GimenezCristian <gimenezc779@gmail.com>
2023-05-09 01:35:57 -0300

    Modificando para que aparezcan las categorias

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from blog.models import Post
-from blog.models import Categoria
+from blog.models import Post, Categoria
+
 
 # Create your views here.
 
 def blog (request):
     
     posts=Post.objects.all()
-    categoria=Categoria.objects.all()
+    
 
-    return render (request, "blog/blog.html", {"posts":posts,"categorias":categoria})
+    return render (request, "blog/blog.html", {"posts":posts})
+
+def categoria(request, categoria_id):
+    categoria = Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categoria)
+    return render(request, "blog/categoria.html", {"categoria": categoria, "posts": posts})
